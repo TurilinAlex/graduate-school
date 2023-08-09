3c1e93b59402cb41cae07836a5473171ab7b0638
turilin <turilin@primatlab.com>
2023-08-09 16:32:25 +0300

    Update

--- run/__main__.py
+++ run/__main__.py
@@ -1,14 +1,21 @@
-import pandas as pd
-from matplotlib import pyplot as plt
+import os
+import time
+from collections import defaultdict
 
+import numpy as np
+import pandas as pd
+from TradingMath.extremal import extremal_max, extremal_min
+from core_math.extremum import coincident, max_extremum, min_extremum
+from core_math.merge_arg_sort import merge_arg_sort
 from core_math.trend_detection import CombinedExtremum, MergeExtremum
+from matplotlib import pyplot as plt
 
 P = 0.9
 N_ROW = 10_000
 
 
 def main_merge():
-    df = pd.read_csv('../data/AUD_USD.csv', nrows=N_ROW)
+    df = pd.read_csv("../data/AUD_USD.csv", nrows=N_ROW)
     close = df.Close.values
     trend_merge = MergeExtremum(close, P)
     trend_merge.plot_values()
@@ -30,7 +37,7 @@ def main_merge():
 
 
 def main_combined():
-    df = pd.read_csv('../data/AUD_USD.csv', nrows=N_ROW)
+    df = pd.read_csv("../data/AUD_USD.csv", nrows=N_ROW)
     close = df.Close.values
     trend_combined = CombinedExtremum(close, P)
     trend_combined.plot_values()
@@ -52,15 +59,40 @@ def main_combined():
 
 
 def main_correlation():
-    df = pd.read_csv('../data/AUD_USD.csv', nrows=N_ROW)
-    close = df.Close.values
+    directory_path = '../data/'
+    for file in os.listdir(directory_path):
+        if os.path.isfile(os.path.join(directory_path, file)):
+            f = os.path.join(directory_path, file)
+            print(f)
+            df_all = pd.read_csv(f, chunksize=N_ROW)
+            statistic = defaultdict(list)
+            df: pd.DataFrame
+            for df in df_all:
+                close = df.Close.values
+                index = merge_arg_sort(close)
+                if len(index) != N_ROW:
+                    break
+                for i in range(1, 4):
 
-    for i in range(1, 3):
-        trend_combined = CombinedExtremum(close, P)
-        trend_combined.main_extremum(num_coincident=1, start_eps=i)
+                    max_index = extremal_max(index=index, eps=i)
+                    min_index = extremal_min(index=index, eps=i)
 
-        print(len(trend_combined.get_combined_values(1)))
+                    statistic[f"max_len_eps_{i}"].append(len(max_index))
+                    statistic[f"min_len_eps_{i}"].append(len(min_index))
+
+                max_index, eps_max = coincident(3)(extremal_max)(index=index, eps=50)
+                min_index, eps_min = coincident(3)(extremal_min)(index=index, eps=50)
+
+                statistic["max_len_eps"].append(len(max_index))
+                statistic["min_len_eps"].append(len(min_index))
+
+                statistic["element"].append(len(index))
+                statistic["max_eps_label"].append(eps_max)
+                statistic["min_eps_label"].append(eps_min)
+
+            st = pd.DataFrame(statistic)
+            st.to_csv(f"eps_{file}", index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main_correlation()
