76149d850a079d78e3383ed466cf498e9ceb5e06
turilin <turilin@primatlab.com>
2023-11-22 10:30:00 +0300

    Update

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,20 +3,20 @@ import pytest
 
 
 @pytest.fixture
-def merge_sort_first_set_data():
+def merge_sort_first_set_data() -> np.ndarray[np.float32]:
     return np.array([0.94, -1.04, -42.24, 12.35, 23.24, 23.24, 0.94, 0.025, 0.0])
 
 
 @pytest.fixture
-def merge_sort_first_set_index():
+def merge_sort_first_set_index() -> np.ndarray[np.uint32]:
     return np.array([2, 1, 8, 7, 0, 6, 3, 4, 5])
 
 
 @pytest.fixture
-def merge_sort_second_set_data():
-    return np.array([0, -1, 4, -1, 0, 0, -6, 2, 6])
+def merge_sort_second_set_data() -> np.ndarray[np.float32]:
+    return np.array([0.0, -1.0, 4.0, -1.0, 0.0, 0.0, -6.0, 2.0, 6.0])
 
 
 @pytest.fixture
-def merge_sort_second_set_index():
+def merge_sort_second_set_index() -> np.ndarray[np.uint32]:
     return np.array([6, 1, 3, 0, 4, 5, 7, 2, 8])
--- tests/test_merge_arg_sort.py
+++ tests/test_merge_arg_sort.py
@@ -1,11 +1,13 @@
-from core_math.merge_arg_sort import merge_arg_sort
+import numpy as np
+
+from core.sort import argsort
 
 
 def test_first_set(merge_sort_first_set_index, merge_sort_first_set_data):
-    sort_index = merge_arg_sort(merge_sort_first_set_data)
-    assert all(merge_sort_first_set_index == sort_index)
+    sort_index = argsort(merge_sort_first_set_data)
+    assert np.all(merge_sort_first_set_index == sort_index)
 
 
 def test_second_set(merge_sort_second_set_index, merge_sort_second_set_data):
-    sort_index = merge_arg_sort(merge_sort_second_set_data)
-    assert all(merge_sort_second_set_index == sort_index)
+    sort_index = argsort(merge_sort_second_set_data)
+    assert np.all(merge_sort_second_set_index == sort_index)
