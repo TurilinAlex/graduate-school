4547fb2044f5ddbf2194cfbd641f206595b5bc7f
turilin <turilin@primatlab.com>
2023-11-22 10:20:35 +0300

    Replace dirs

---
+++ third_party/TradingMath/extremal.pyi
@@ -0,0 +1,7 @@
+from numpy import uint32, ndarray
+
+
+def extremal_max(index: ndarray[uint32], eps: int) -> ndarray[uint32]: ...
+
+
+def extremal_min(index: ndarray[uint32], eps: int) -> ndarray[uint32]: ...
